f158cff3b724e804ccc8869eff86d89dc01bf4a6
Vindhuja2023 <vindhuja.rajeev@gmail.com>
2023-04-13 12:42:16 -0400

    second commit

---
+++ src/pizzaProgram/MainPizzaBill.java
@@ -0,0 +1,16 @@
+package pizzaProgram;
+
+
+
+public class MainPizzaBill {
+
+	public static void main(String[] args) {
+		PizzaBill pizza = new PizzaBill();
+		String[] pizzaSize = {"Small","Medium","Large"};
+		pizza.displayingPizzaDetails(pizzaSize);
+		pizza.bill(pizzaSize);
+
+
+	}
+
+}
